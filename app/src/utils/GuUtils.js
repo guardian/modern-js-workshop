ddc55011dd1c3296198440c9d779f28e9450a694
shaundillon <shaun.dillon@guardian.co.uk>
2018-03-06 17:45:04 +0000

    add first test and deps

--- app/src/utils/GuUtils.js
+++ app/src/utils/GuUtils.js
@@ -1,5 +1,5 @@
 // Create a query string from an object
-const GuUtils = {
+export const GuUtils = {
   objectToQueryString: obj =>
   `?${Object.entries(obj)
     .map(([k, v]) => `${encodeURI(k)}=${encodeURI(v)}`)
