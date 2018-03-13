fd182dac982e71a325f482d6ff59a147764fec10
Alex Sanders <a@snd.rs>
2018-03-13 17:30:49 +0000

    add webpack

--- app/src/index.js
+++ app/src/index.js
@@ -1,48 +1,56 @@
 /*
   --- The App ---
 */
+import GuDOM from './lib/GuDom';
+import { CAPI } from './services/CAPI';
 
 const { div, header, main, footer, nav, h1, h2, ul, li, a, p } = GuDOM;
 
 /* Run the app */
 
 (async () => {
-  const { response: { results } } = await CAPI.fetchLatest(10);
+    const { response: { results } } = await CAPI.fetchLatest(10);
 
-  const vDOM =
-    div({ class: "app" },
-      header({},
-        h1({},
-          "The Guardian"
-        )
-      ),
-      main({},
-        div({},
-          h2({},
-            "News"
-          ),
-          nav({ role: "navigation" },
-            ul({},
-              ...results.map(({ webTitle, webUrl, sectionName }) =>
-                li({},
-                  a({ href: webUrl, title: webTitle, target: "_blank", rel: "noopener" },
-                    `${sectionName} : ${webTitle}`
-                  )
+    const vDOM = div(
+        { class: 'app' },
+        header({}, h1({}, 'The Guardian')),
+        main(
+            {},
+            div(
+                {},
+                h2({}, 'News'),
+                nav(
+                    { role: 'navigation' },
+                    ul(
+                        {},
+                        ...results.map(({ webTitle, webUrl, sectionName }) =>
+                            li(
+                                {},
+                                a(
+                                    {
+                                        href: webUrl,
+                                        title: webTitle,
+                                        target: '_blank',
+                                        rel: 'noopener',
+                                    },
+                                    `${sectionName} : ${webTitle}`
+                                )
+                            )
+                        )
+                    )
                 )
-              )
             )
-          )
+        ),
+        footer(
+            {},
+            p(
+                {},
+                `© The Guardian ${new Date().toLocaleDateString('en-GB', {
+                    year: 'numeric',
+                })}`
+            )
         )
-      ),
-      footer({},
-        p({},
-          `© The Guardian ${new Date().toLocaleDateString("en-GB", { year: "numeric" })}`
-        )
-      )
     );
 
-  GuDOM.render(
-    vDOM,
-    document.getElementById("app")
-  );
+    GuDOM.render(vDOM, document.getElementById('app'));
 })();
--- app/src/lib/GuDOM.js
+++ app/src/lib/GuDOM.js
@@ -3,7 +3,7 @@
 */
 
 const GuDOM = (() => {
-  /*
+    /*
   createDOMNodeEl
 
   Returns a factory function for creating Virtual DOM elements with any
@@ -37,77 +37,79 @@ const GuDOM = (() => {
   }
   */
 
-  const createDOMNodeEl = tag => (attrs = {}, ...children) => ({
-    tag,
-    attrs,
-    children
-  });
-
-  /*
-    This function just uses an object to setAttributes on a DOM node
-  */
-  const setElementAttributes = (el, attrs = {}) =>
-    Object.entries(attrs).forEach(([attr, val]) => {
-      el.setAttribute(attr, val);
+    const createDOMNodeEl = tag => (attrs = {}, ...children) => ({
+        tag,
+        attrs,
+        children,
     });
 
-  /*
+    /*
+    This function just uses an object to setAttributes on a DOM node
+  */
+    const setElementAttributes = (el, attrs = {}) =>
+        Object.entries(attrs).forEach(([attr, val]) => {
+            el.setAttribute(attr, val);
+        });
+
+    /*
   The render function takes a VDOM representation and recurses through the nodes
   (through the "children" property) and creates and appends the actual DOM nodes
   */
 
-  const render = (vDOMEl, node) => {
-    let el;
-    if (typeof vDOMEl === "string") {
-      el = document.createTextNode(vDOMEl);
-    } else {
-      const { tag, attrs, children = [] } = vDOMEl;
-      el = document.createElement(tag);
-      setElementAttributes(el, attrs);
-      children.forEach(vDOMChild => render(vDOMChild, el));
-    }
-    node.appendChild(el);
-  };
+    const render = (vDOMEl, node) => {
+        let el;
+        if (typeof vDOMEl === 'string') {
+            el = document.createTextNode(vDOMEl);
+        } else {
+            const { tag, attrs, children = [] } = vDOMEl;
+            el = document.createElement(tag);
+            setElementAttributes(el, attrs);
+            children.forEach(vDOMChild => render(vDOMChild, el));
+        }
+        node.appendChild(el);
+    };
 
-  /*
+    /*
   These strings will be used to create functions of the same name that create
   vDOM nodes that represent tags of the same name!.
   */
-  const TAG_NAMES = [
-    "div",
-    "header",
-    "main",
-    "footer",
-    "nav",
-    "h1",
-    "h2",
-    "ul",
-    "li",
-    "a",
-    "p"
-  ];
+    const TAG_NAMES = [
+        'div',
+        'header',
+        'main',
+        'footer',
+        'nav',
+        'h1',
+        'h2',
+        'ul',
+        'li',
+        'a',
+        'p',
+    ];
 
-  /*
+    /*
   createDOMNodes
 
   Takes an array of DOM node names and runs createDOMNodeEl on them to
   create an object with each key being a VDOM factory for that tag
   */
 
-  const createDOMNodes = tags =>
-    tags.reduce(
-      (out, tag) => ({
-        ...out,
-        [tag]: createDOMNodeEl(tag)
-      }),
-      {}
-    );
+    const createDOMNodes = tags =>
+        tags.reduce(
+            (out, tag) => ({
+                ...out,
+                [tag]: createDOMNodeEl(tag),
+            }),
+            {}
+        );
 
-  /*
+    /*
   These are now are the VDOM factories to use in our app!
   */
-  return {
-    ...createDOMNodes(TAG_NAMES),
-    render
-  };
+    return {
+        ...createDOMNodes(TAG_NAMES),
+        render,
+    };
 })();
+
+export default GuDOM;
